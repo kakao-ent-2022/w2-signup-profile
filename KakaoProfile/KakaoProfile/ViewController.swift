5f6dfdcc2eb49e0ad261ed9d04883a7bef82c697
terry <terry.yes@kakaoent.com>
2022-01-17 13:14:58 +0900

    feat: 첫번째 탭바 화면 구성(image, label, button)

--- KakaoProfile/KakaoProfile/ViewController.swift
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(#file, #line, #function, #column)
+        self.nameLabel.text = "JK"
+        self.descriptionLabel.text = "카르페 디엠"
     }
 
-
+    @IBAction func editButtonTouched(_ sender: Any) {
+        self.nameLabel.textColor = UIColor.blue
+        self.nameLabel.backgroundColor = UIColor.yellow
+        self.nameLabel.alpha = 0.5
+        self.descriptionLabel.text = "크루미션"
+    }
+    
 }
 
