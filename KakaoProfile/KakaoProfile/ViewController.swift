cba940f5ef137c5d39a09e01229c8d0a57730db7
lauren.c <lauren.c@kakaoent.com>
2022-01-17 15:59:37 +0900

    [step1] tab bar controller 추가

---
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KakaoProfile
+//
+//  Created by lauren.c on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
