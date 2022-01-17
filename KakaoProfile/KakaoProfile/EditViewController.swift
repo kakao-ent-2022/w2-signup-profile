37a86b3cd791159d6455f74b74aae8a997b1233a
terry <terry.yes@kakaoent.com>
2022-01-17 16:21:13 +0900

    feat: 뷰컨트롤러 생명주기별 함수에 "print(#file, #line, #function, #column)" 내용 추가

--- KakaoProfile/KakaoProfile/EditViewController.swift
+++ KakaoProfile/KakaoProfile/EditViewController.swift
@@ -38,7 +38,23 @@ class EditViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
 
 }
