a30dfd251a47b88ecc0f0cc3e5ff75442f327f3b
river-o <river.o@kakaoent.com>
2022-01-18 13:48:30 +0900

    feat: Button Touch Event 생성

--- KakaoProfile/KakaoProfile/ProfileViewController.swift
+++ KakaoProfile/KakaoProfile/ProfileViewController.swift
@@ -13,6 +13,13 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    @IBAction func editButtonTouched(_ sender: Any) {
+        self.nameLabel.textColor = UIColor.blue
+        self.nameLabel.backgroundColor = UIColor.yellow
+        self.nameLabel.alpha = 0.5
+        self.descriptionLabel.text = "크루미션"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
