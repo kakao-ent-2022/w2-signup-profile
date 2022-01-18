889ac63ed30f7e27d3f4000327b22e2abaec34ac
terry <terry.yes@kakaoent.com>
2022-01-18 15:02:46 +0900

    feat: login화면에서 id와 pw가 "kakao"일때만 다음 화면으로 넘어가도록 구현

--- KakaoProfile/KakaoProfile/LoginViewController.swift
+++ KakaoProfile/KakaoProfile/LoginViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if idTextField.text == "kakao" &&
+            passwordTextField.text == "kakao" {
+            return true
+        }
+        return false
     }
-    */
-
+    
 }
