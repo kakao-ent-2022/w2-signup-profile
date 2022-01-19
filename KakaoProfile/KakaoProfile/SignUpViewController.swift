47bba49db3ccf3aaef57b01dfc8233f08d7e49cc
terry <terry.yes@kakaoent.com>
2022-01-19 17:08:04 +0900

    feat: 회원가입 화면의 각 textFieldView에 맞는 이름, 캡션, placeholder설정

--- KakaoProfile/KakaoProfile/SignUpViewController.swift
+++ KakaoProfile/KakaoProfile/SignUpViewController.swift
@@ -8,8 +8,22 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var idTextView: SignUpTextFieldView!
+    @IBOutlet weak var passwordTextView: SignUpTextFieldView!
+    @IBOutlet weak var passwordCheckTextView: SignUpTextFieldView!
+    @IBOutlet weak var nameTextView: SignUpTextFieldView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateTextView()
+    }
+    
+    func updateTextView() {
+        idTextView.updateTitle(title: "아이디")
+        idTextView.updatePlaceholder(placeHolder: "영문 소문자, 숫자, 특수기호(_,-), 5~20자")
+        passwordTextView.updateTitle(title: "비밀번호")
+        passwordTextView.updatePlaceholder(placeHolder: "영문 대/소문자, 숫자, 특수문자(!@#$%) 8~16자")
+        passwordCheckTextView.updateTitle(title: "비밀번호 재확인")
+        nameTextView.updateTitle(title: "이름")
     }
 }
--- KakaoProfile/KakaoProfile/View/SignUpTextFieldView.swift
+++ KakaoProfile/KakaoProfile/View/SignUpTextFieldView.swift
@@ -9,23 +9,45 @@ import UIKit
 
 class SignUpTextFieldView: UIView {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var captionLabel: UILabel!
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.loadXib()
+        updateInitialStyle()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.loadXib()
+        updateInitialStyle()
+    }
+    
+    func updateInitialStyle() {
+        updateCaption(caption: "")
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    func updateTitle(title: String) {
+        self.titleLabel.text = title
+    }
+    
+    func updateCaption(caption: String) {
+        self.captionLabel.text = caption
+    }
+    
+    func updatePlaceholder(placeHolder: String) {
+        self.textField.placeholder = placeHolder
     }
     
     private func loadXib() {
         let identifier = String(describing: type(of: self))
         let nibs = Bundle.main.loadNibNamed(identifier, owner: self, options: nil)
-        
         guard let SignUpTextFieldView = nibs?.first as? UIView else { return }
         SignUpTextFieldView.frame = self.bounds
         self.addSubview(SignUpTextFieldView)
     }
-
 }
