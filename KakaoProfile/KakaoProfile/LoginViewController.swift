34360ded8bf670398503f876a617db1b5cdb884f
river-o <river.o@kakaoent.com>
2022-01-19 21:34:55 +0900

    feat: 로그인 성공 시 Main 화면 이동

--- KakaoProfile/KakaoProfile/LoginViewController.swift
+++ KakaoProfile/KakaoProfile/LoginViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    let onLoginSuccessSegue = "onLoginSuccess"
+    let id = "kakao"
+    let password = "1234"
 
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var idEditText: UITextField!
     @IBOutlet weak var passwordEditText: UITextField!
+    
+    @IBAction func selectLoginButtonTouched(_ sender: Any) {
+        let idText = idEditText.text
+        let passwordText = passwordEditText.text
+        
+        if(idText == id && passwordText == password) {
+            self.performSegue(withIdentifier: onLoginSuccessSegue, sender: nil)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ KakaoProfile/KakaoProfile/MainViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewController.swift
+//  KakaoProfile
+//
+//  Created by river.o on 2022/01/19.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+    
+}
