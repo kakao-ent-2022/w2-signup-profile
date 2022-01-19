eccc21412b44cb522c33c3011385078cb06417e6
terry <terry.yes@kakaoent.com>
2022-01-19 10:20:38 +0900

    feat: 탭바뷰컨트롤러전에 회원가입 씬 생성

---
+++ KakaoProfile/KakaoProfile/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.swift
+//  KakaoProfile
+//
+//  Created by terry.yes on 2022/01/19.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+    }
+
+}
