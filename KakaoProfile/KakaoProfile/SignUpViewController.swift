b561da754289c940fe7a4ba14e46588cee97dfef
river-o <river.o@kakaoent.com>
2022-01-24 02:53:11 +0900

    feat: 회원가입 시 Form Validation

--- KakaoProfile/KakaoProfile/SignUpViewController.swift
+++ KakaoProfile/KakaoProfile/SignUpViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+enum SignUpItem : Int, RawRepresentable {
+    case id = 0
+    case password = 1
+    case passwordCheck = 2
+    case name = 3
+}
 
-    @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var passwordCheckTextField: UITextField!
-    @IBOutlet weak var nameTextField: UITextField!
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var idTextField: UITextField! {
+        didSet{
+            idTextField.tag = SignUpItem.id.rawValue
+            idTextField.delegate = self
+        }
+    }
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet{
+            passwordTextField.tag = SignUpItem.password.rawValue
+            passwordTextField.delegate = self
+        }
+    }
+    @IBOutlet weak var passwordCheckTextField: UITextField! {
+        didSet{
+            passwordCheckTextField.tag = SignUpItem.passwordCheck.rawValue
+            passwordCheckTextField.delegate = self
+        }
+    }
+    @IBOutlet weak var nameTextField: UITextField! {
+        didSet{
+            nameTextField.tag = SignUpItem.name.rawValue
+            nameTextField.delegate = self
+        }
+    }
     
+    @IBOutlet weak var idInformationLabel: UILabel!
+    @IBOutlet weak var passwordInformationLabel: UILabel!
+    @IBOutlet weak var passwordCheckInformationLabel: UILabel!
+    @IBOutlet weak var nameInformationLabel: UILabel!
+    
+    private lazy var signUpTextFields = [idTextField, passwordTextField, passwordCheckTextField, nameTextField]
+    private lazy var signUpLabels = [idInformationLabel, passwordInformationLabel, passwordCheckInformationLabel, nameInformationLabel]
+    
+    private let signUpValidation = SignUpValidation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +60,42 @@ class SignUpViewController: UIViewController {
     private func initUI() {
         idTextField.placeholder = "영문 소문자, 숫자, 특수기호(_,-), 5~20자"
         passwordTextField.placeholder = "영문 대/소문자, 숫자, 특수문자(!@#$%) 8~16자"
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let tag = textField.tag
+        if let text = textField.text {
+            do {
+                if tag != SignUpItem.name.rawValue && text.isEmpty {
+                    textField.layer.borderWidth = 0.0
+                    signUpLabels[tag]?.text = ""
+                    return
+                } else {
+                    textField.layer.borderWidth = 1.0
+                }
+                
+                let compareText = (tag == SignUpItem.passwordCheck.rawValue) ? passwordTextField.text : nil
+                let signUpInformation = try signUpValidation.getValidationInformation(tag: tag, text: text, compareText: compareText)
+                setSignUpInformationLabel(text: signUpInformation, tag: tag, isValidate: true)
+            } catch SignUpValidationError.InvaildSignUpText(let errorMessage) {
+                setSignUpInformationLabel(text: errorMessage, tag: tag, isValidate: false)
+            } catch {
+            }
+        }
+    }
+    
+    private func setSignUpInformationLabel(text: String, tag: Int, isValidate: Bool) {
+        let textField = signUpTextFields[tag]
+        let label = signUpLabels[tag]
+        
+        label?.text = text
+        
+        if(isValidate) {
+            label?.textColor = UIColor.green
+            textField?.layer.borderColor = UIColor.green.cgColor
+        } else {
+            label?.textColor = UIColor.red
+            textField?.layer.borderColor = UIColor.red.cgColor
+        }
     }
 }
