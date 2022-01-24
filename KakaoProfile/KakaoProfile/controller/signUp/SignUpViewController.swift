0a78f620cf5a5ad6f2bf0b0b44b822f8bc22585f
nylah-j <nylah.j@kakaoent.com>
2022-01-24 10:44:24 +0900

    [FEAT] ValidatinoTextField 이용하여 SignUpViewController 구현

--- KakaoProfile/KakaoProfile/controller/signUp/SignUpViewController.swift
+++ KakaoProfile/KakaoProfile/controller/signUp/SignUpViewController.swift
@@ -9,46 +9,69 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
-    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var idValidationTextView: ValidationTextView!
+    @IBOutlet weak var passwordValidationTextView: ValidationTextView!
+    @IBOutlet weak var rePasswordValidationTextView: StandardValidationTextView!
+    @IBOutlet weak var nameValidationTextView: ValidationTextView!
     
-    @IBOutlet weak var idField: UITextField!
-    @IBOutlet weak var idCautionLabel: UILabel!
-    
-    @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var passwordCautionLabel: UILabel!
-    
-    @IBOutlet weak var rePasswordField: UITextField!
-    @IBOutlet weak var rePasswordCautionLabel: UILabel!
-    
-    @IBOutlet weak var nameField: UITextField!
-    @IBOutlet weak var nameCautionLabel: UILabel!
-    
-
     private var isIdValid: Bool = false
     private var isPasswordValid: Bool = false
     private var isRePasswordSame: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (hasLoginHistory()) {
+            navigateToLogin()
+        }
+       
+        initIdValidationTextView()
+        initPasswordValidationTextView()
+        initRePasswordValidationTextView()
+        initNameValidationTextView()
+    }
+    
+    func hasLoginHistory() -> Bool {
+        guard let _ = UserStorage.get()
+        else {
+            return false
+        }
+        return true
+    }
+    
+    func initIdValidationTextView() {
+        idValidationTextView.setTitle(title: "아이디")
+        idValidationTextView.setInputPlaceHolder(placeHolder: "영문 소문자, 숫자, 특수기호 (_, -), 5~20자")
+        idValidationTextView.validationDelgate = IdValidationDelegate()
+    }
+    
+    func initPasswordValidationTextView() {
+        passwordValidationTextView.setTitle(title: "비밀번호")
+        passwordValidationTextView.setInputPlaceHolder(placeHolder: "영문 대/소문자, 숫자, 특수문자!@#$%) 8~16자")
+        passwordValidationTextView.validationDelgate = PasswordValidationDelegate()
+    }
+    
+    func initRePasswordValidationTextView() {
+        rePasswordValidationTextView.setTitle(title: "비밀번호 재확인")
+        rePasswordValidationTextView.validationDelgate = RePasswordValidationDelegate()
+    }
+    
+    func initNameValidationTextView() {
+        nameValidationTextView.setTitle(title: "이름")
+        nameValidationTextView.validationDelgate = NameValidationDelgate()
     }
     
     @IBAction func onNextButtonTouched(_ sender: UIButton) {
-        let isNameValid = checkName(name: nameField.text)
-        
-        if !isNameValid
-            || !isIdValid
-            || !isPasswordValid
-            || !isRePasswordSame {
+        guard let id = idValidationTextView.getValidInputOrNil(),
+              let password = passwordValidationTextView.getValidInputOrNil(),
+              let _ = rePasswordValidationTextView.getValidInputOrNil(),
+              let name = nameValidationTextView.getValidInputOrNil()
+        else {
             return
         }
-        
-        guard let id = idField.text,
-              let password = passwordField.text else {
-            return
-        }
-        
+    
         Task.init {
-            let isSignUpSuccess = await trySingUp(id: id, password: password)
+            let isSignUpSuccess = await trySingUp(id: id, password: password, name: name)
             
             if(isSignUpSuccess) {
                 navigateToLogin()
@@ -57,17 +80,18 @@ class SignUpViewController: UIViewController {
     }
     
     private func navigateToLogin() {
+        self.transitioningDelegate = self
         let controller = storyboard?.instantiateViewController(withIdentifier: "loginViewController")
         controller?.transitioningDelegate = self
         controller?.modalPresentationStyle = .custom
         present(controller!, animated: true)
     }
     
-    private func trySingUp(id: String, password: String) async -> Bool {
+    private func trySingUp(id: String, password: String, name: String) async -> Bool {
         do {
             try await NetworkHandler.postSignUp(id: id, password: password)
             
-            guard let user = User(id: id, password: password) else {
+            guard let user = User(name: name, id: id, password: password) else {
                 throw NetworkError.paramsError
             }
             UserStorage.save(user: user)
@@ -76,122 +100,15 @@ class SignUpViewController: UIViewController {
         } catch NetworkError.paramsError  {
             print("Network Error: id or passwor is empty")
         } catch NetworkError.duplicateId {
-            idCautionLabel.text = IdValidationResult.duplication.value
+            idValidationTextView.setCautionByValidationResult(result: IdValidationResult.duplication)
 
         } catch NetworkError.parseError {
             print("Network Error: URL Parse Error")
         } catch {
             print("Network Error: UnKnown Error Occured")
         }
+        
         return false
-    }
-    
-    private func checkName(name: String?) -> Bool {
-        guard let name = nameField.text,
-              name.count > 0
-        else {
-            nameCautionLabel.isHidden = false
-            return false
-        }
-        
-        nameCautionLabel.isHidden = true
-        return true
-    }
-
-    @IBAction func onIdFieldChanging(_ sender: UITextField) {
-        isIdValid = IdChecker.checkId(id: sender.text)
-        
-        idCautionLabel.isHidden = false
-        
-        if isIdValid {
-            idCautionLabel.text = IdValidationResult.usableness.value
-            idCautionLabel.textColor = UIColor.green
-            return
-        }
-        
-        idCautionLabel.text = IdValidationResult.invalidFormat.value
-        idCautionLabel.textColor = UIColor.red
-    }
-    
-    
-    @IBAction func onPasswordFieldChanging(_ sender: UITextField) {
-        passwordCautionLabel.isHidden = false
-        
-        guard let password = sender.text,
-              PasswordChecker.checkSize(password: password)
-        else {
-            setPasswordCaution(content: .invalidSize)
-            isPasswordValid = false
-            return
-        }
-        
-        if(!PasswordChecker.checkHasUpper(password: password)) {
-            setPasswordCaution(content: .uppercaseNonExist)
-            isPasswordValid = false
-            return
-        }
-        
-        if(!PasswordChecker.checkHasNumber(password: password)) {
-            setPasswordCaution(content: .numberNonExist)
-            isPasswordValid = false
-            return
-        }
-        
-        if(!PasswordChecker.checkHasSpecialSymbol(password: password)) {
-            setPasswordCaution(content: .specialSymbolNonExist)
-            isPasswordValid = false
-            return
-        }
-                
-        setPasswordCaution(content: .usableness)
-        isPasswordValid = true
-    }
-    
-    private func setPasswordCaution(content: PasswordCautionContent) {
-        passwordCautionLabel.text = content.value
-        
-        switch content {
-        case .usableness:
-            passwordCautionLabel.textColor = UIColor.green
-        case .invalidSize,
-            .numberNonExist,
-            .specialSymbolNonExist,
-            .uppercaseNonExist:
-            passwordCautionLabel.textColor = UIColor.red
-        }
-    }
-    
-    @IBAction func onRePasswordFieldChanging(_ sender: UITextField) {
-        rePasswordField.isHidden = false
-        
-        guard let password = passwordField.text,
-              let rePassword = sender.text
-        else {
-            setRePasswordCaution(content: RePasswordCautionContent.nonMatch)
-            isRePasswordSame = false
-            return
-        }
-        
-        if password != rePassword {
-            setRePasswordCaution(content: RePasswordCautionContent.nonMatch)
-            isRePasswordSame = false
-            return
-        }
-        
-        setRePasswordCaution(content: .match)
-        isRePasswordSame = true
-        return
-    }
-    
-    private func setRePasswordCaution(content: RePasswordCautionContent) {
-        rePasswordCautionLabel.text = content.value
-        
-        switch content {
-        case .match:
-            rePasswordCautionLabel.textColor = UIColor.green
-        case .nonMatch:
-            rePasswordCautionLabel.textColor = UIColor.red
-        }
     }
 }
 
